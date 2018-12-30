ebbf26482c30ff15217130705a410e33062635d2
fudon <fuart@qq.com>
2018-12-31 01:14:26 +0800

    1.0.6.5

--- sdks/FSAccount/classes/FSBestUpdateController.h
+++ sdks/FSAccount/classes/FSBestUpdateController.h
@@ -2,7 +2,7 @@
 //  FSBestUpdateController.h
 //  FSKit_Example
 //
-//  Created by Guazi on 2018/6/27.
+//  Created by FudonFuchina on 2018/6/27.
 //  Copyright © 2018年 topchuan. All rights reserved.
 //
 
@@ -14,6 +14,6 @@
 @property (nonatomic,strong) FSBestAccountModel     *model;
 @property (nonatomic,copy) NSString                 *table;
 @property (nonatomic,assign) NSInteger              isAJ;
-@property (nonatomic,copy) void (^updatedCallback)(void);
+@property (nonatomic,copy) void (^updatedCallback)(BOOL needRefresh);
 
 @end
